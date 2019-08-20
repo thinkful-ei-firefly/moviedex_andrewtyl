371a88f723bd33ab1890237a772a6f0726ff85f5
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-08-20 16:26:29 -0500

    authorization complete

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const helmet = require('helmet');
 const morgan = require('morgan');
 const movieDatabase = require('./dev-movies-data');
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized request'})
+    }
+    next()
+})
+
 
 app.get('/', (req, res) => {
     res
